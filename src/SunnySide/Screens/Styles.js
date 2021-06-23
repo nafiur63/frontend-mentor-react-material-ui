c62a35c0b411bb53dfd890effa6ee4e94a0adc7f
nafiur63 <nafiur63@gmail.com>
2021-06-24 04:04:36 +0600

    second day

--- src/SunnySide/Screens/Styles.js
+++ src/SunnySide/Screens/Styles.js
@@ -1,8 +1,16 @@
 import {makeStyles} from "@material-ui/core/styles"
-import HeaderBack from './image-header.jpg'
-import MobHeaderBack from './mob-image-header.jpg'
-import EggDeskBack from './eggdesk.jpg'
-import EggMobBack from './eggmobile.jpg'
+import HeaderBack from './images/image-header.jpg'
+import MobHeaderBack from './images/mob-image-header.jpg'
+import EggDeskBack from './images/eggdesk.jpg'
+import EggMobBack from './images/eggmobile.jpg'
+import StickDeskBack from './images/stickdesk.jpg'
+import StickMobBack from './images/stickmobile.jpg'
+import CherryDeskBack from './images/cherrydesk.jpg'
+import CherryMobBack from './images/cherrymobile.jpg'
+import OrangeDeskBack from './images/parsimondesk.jpg'
+import OrangeMobBack from './images/parsimonmobile.jpg'
+
+
 
 
 export default makeStyles(theme => ({
@@ -25,17 +33,51 @@ export default makeStyles(theme => ({
         }
     },
 
+    creativeHeader: {
+        
+        margin:"9rem 0 0 0"
+    },
 
     arrowIcon:{
         position:"absolute",
         top:"60%",
         left:"45%",
-        [theme.breakpoints.up('sm')]: {
+        [theme.breakpoints.up('md')]: {
         position:"absolute",
         top:"45%",
         left:"48%",
         }
     },
+
+    doubleBox:{
+        height:"100vh",
+        [theme.breakpoints.up('md')]: {
+            height:"80vh",
+            }
+    },
+    
+    // MuiTypographybody1:{
+    //     fontFamily: "Big Shoulders Stencil Display, cursive",
+    // },
+
+    // body1: {
+        // "& .MuiBottomNavigationAction-root": {
+        //     minWidth: 0,
+        //     maxWidth: 250
+        // },
+        // "& .MuiSvgIcon-root": {
+        //     fill:"white",
+        //     "&:hover": {
+        //         fill:"tomato",
+        //         fontSize:"2rem"
+        //     }
+        // }
+    //     " .MuiTypography-body1":{
+    //         fontFamily: "Big Shoulders Stencil Display, cursive",
+    //         fontSize:"40px",
+    //     },
+
+    // },
 
     navbar: {
         // [theme.breakpoints.up("sm")]: {
@@ -47,7 +89,7 @@ export default makeStyles(theme => ({
         backgroundImage: `url(${EggMobBack})`,
         backgroundRepeat:"no-repeat",
         backgroundSize: "cover",
-        [theme.breakpoints.up('sm')]: {
+        [theme.breakpoints.up('md')]: {
           order:"2",
             
           backgroundImage: `url(${EggDeskBack})`,
@@ -58,24 +100,69 @@ export default makeStyles(theme => ({
       item1: {
         order: "2",
         
-        [theme.breakpoints.up('sm')]: {
+        [theme.breakpoints.up('md')]: {
           order:"1",
         },
       },
-      EggBackground:{
-        backgroundImage: `url(${EggMobBack})`,
-        backgroundRepeat:"no-repeat",
-        backgroundSize: "cover",
+    //   EggBackground:{
+    //     backgroundImage: `url(${EggMobBack})`,
+    //     backgroundRepeat:"no-repeat",
+    //     backgroundSize: "cover",
 
-        [theme.breakpoints.up("sm")]: {
-        backgroundImage: `url(${EggDeskBack})`,
-        backgroundRepeat:"no-repeat",
-        backgroundSize: "cover",
-        }
-      },
+    //     [theme.breakpoints.up("md")]: {
+    //     backgroundImage: `url(${EggDeskBack})`,
+    //     backgroundRepeat:"no-repeat",
+    //     backgroundSize: "cover",
+    //     }
+    //   },
       textBox:{
           display:"flex",
           justifyContent:"center",
           alignItems:"center"
-      }
+      },
+      stickBackground:{
+        backgroundImage: `url(${StickMobBack})`,
+        backgroundRepeat:"no-repeat",
+        backgroundSize: "cover",
+
+        [theme.breakpoints.up("md")]: {
+        backgroundImage: `url(${StickDeskBack})`,
+        backgroundRepeat:"no-repeat",
+        backgroundSize: "cover",
+        }
+      },
+      cherryBackground:{
+        position:"relative",
+        backgroundImage: `url(${CherryMobBack})`,
+        backgroundRepeat:"no-repeat",
+        backgroundSize: "cover",
+        height:"100%",
+
+        [theme.breakpoints.up("md")]: {
+        backgroundImage: `url(${CherryDeskBack})`,
+        backgroundRepeat:"no-repeat",
+        backgroundSize: "cover",
+        height:"100%",
+        }
+      },
+      orangeBackground:{
+        position:"relative",
+        backgroundImage: `url(${OrangeMobBack})`,
+        backgroundRepeat:"no-repeat",
+        backgroundSize: "100% 100%",
+        // height:"500px",
+        height:"100%",
+
+        [theme.breakpoints.up("md")]: {
+        backgroundImage: `url(${OrangeDeskBack})`,
+        backgroundRepeat:"no-repeat",
+        backgroundSize: "cover",
+        height:"100%",
+        
+        }
+      },
+      photoBanner:{
+        position:"absolute",
+        bottom:"20px",
+      },
 }))
--- src/SunnySide/Screens/SunnySide.js
+++ src/SunnySide/Screens/SunnySide.js
@@ -1,33 +1,43 @@
 import { AppBar, Box, Grid, Link, Typography } from "@material-ui/core";
 import React from "react";
-import logo from "../sunnyside-agency-landing-page-main/sunnyside-agency-landing-page-main/images/logo.svg";
 import headerImage from "../images/desktop/image-header.jpg";
 // import './styles.css'
 
-import downIcon from './icon-arrow-down.svg'
+import downIcon from "./images/icon-arrow-down.svg";
 import useStyles from "./Styles";
 
 const SunnySide = () => {
   const styles = useStyles();
   return (
-    <Box height="100vh">
-      <Box className={styles.header} height="100vh">
+    <Box >
+      {/* first div */}
+
+      <Box className={styles.header} height="100vh" textAlign="center">
         <AppBar
           className={styles.navbar}
           position="relative"
           color="transparent"
           style={{ boxShadow: "none" }}
         >
-          Aula Jhaula Maula Pagla Goru Murgi
+          hola
+          {/* <img style={{}} src={Logo} alt="Sunnyside logo"/>  */}
         </AppBar>
-        <Typography variant="h2" style={{fontFamily:"Fraunces", fontWeight:"900",color:"#ffff"}}>
-            We are creatives
+        <Typography
+          textAlign="center"
+          className={styles.creativeHeader}
+          variant="h2"
+          style={{ fontFamily: "Fraunces", fontWeight: "900", color: "#ffff" }}
+        >
+          We are creatives
         </Typography>
         <img src={downIcon} className={styles.arrowIcon}></img>
       </Box>
-      <Box display="flex" height="80vh">
+
+      {/* second div  */}
+
+      <Box display="flex" className={styles.doubleBox}>
         <Grid container>
-          <Grid className={styles.item1} item sm={6} xs={12}>
+          <Grid className={styles.item1} item md={6} xs={12}>
             <Box
               display="flex"
               justifyContent="center"
@@ -36,8 +46,18 @@ const SunnySide = () => {
               height="100%"
             >
               <Box>
-                <Typography variant="h4">Transform your brand</Typography>
-                <Typography>
+                <Typography
+                  variant="h4"
+                  style={{ fontFamily: "Fraunces", fontWeight: "900" }}
+                  gutterBottom
+                >
+                  Transform your brand
+                </Typography>
+                <Typography
+                  variant="body1"
+                  style={{ fontFamily: "Barlow" }}
+                  gutterBottom
+                >
                   We are a full-service creative agency specializing in helping
                   brands grow fast. Engage your clients through compelling
                   visuals that do most of the marketing for you.
@@ -51,6 +71,139 @@ const SunnySide = () => {
           </Grid>
         </Grid>
       </Box>
+
+      {/* thid div  */}
+
+      <Box display="flex" className={styles.doubleBox}>
+        <Grid container>
+          <Grid className={styles.stickBackground} item md={6} xs={12}></Grid>
+          <Grid item md={6} xs={12}>
+            <Box
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+              textAlign="center"
+              height="100%"
+            >
+              <Box>
+                <Typography
+                  variant="h4"
+                  style={{ fontFamily: "Fraunces", fontWeight: "900" }}
+                  gutterBottom
+                >
+                  Stand out to the right audience
+                </Typography>
+                <Typography style={{ fontFamily: "Barlow" }} gutterBottom>
+                  Using a collaborative formula of designers, researchers,
+                  photographers, videographers, and copywriters, we’ll build and
+                  extend your brand in digital places.
+                </Typography>
+                <Link>Learn more</Link>
+              </Box>
+            </Box>
+          </Grid>
+        </Grid>
+      </Box>
+
+      {/* fourth div  */}
+
+      <Box display="flex" className={styles.doubleBox}>
+        <Grid container style={{height:"100%"}}>
+          <Grid  item md={6} xs={12}>
+            <Box className={styles.cherryBackground}>
+            <Box className={styles.photoBanner} textAlign="center">
+              <Box>
+                <Typography
+                  variant="h4"
+                  style={{ fontFamily: "Fraunces", fontWeight: "900" }}
+                  gutterBottom
+                >
+                  Graphic Design
+                </Typography>
+                <Typography style={{ fontFamily: "Barlow" }} gutterBottom>
+                  Great design makes you memorable. We deliver artwork that
+                  underscores your brand message and captures potential clients’
+                  attention.
+                </Typography>
+                <Link>Learn more</Link>
+              </Box>
+            </Box>
+            </Box>
+            
+          </Grid>
+          <Grid  item md={6} xs={12}>
+              <Box className={styles.orangeBackground}> 
+              <Box className={styles.photoBanner} textAlign="center">
+              <Typography
+                variant="h4"
+                style={{ fontFamily: "Fraunces", fontWeight: "900" }}
+                gutterBottom
+              >
+                Graphic Design
+              </Typography>
+              <Typography style={{ fontFamily: "Barlow" }} gutterBottom>
+                Great design makes you memorable. We deliver artwork that
+                underscores your brand message and captures potential clients’
+                attention.
+              </Typography>
+              <Link>Learn more</Link>
+            </Box>
+              </Box>
+            
+          </Grid>
+        </Grid>
+      </Box>
+
+
+      {/* <Box display="flex" className={styles.doubleBox}>
+        <Grid container>
+          <Grid className={styles.cherryBackground} item md={6} xs={12}>
+            <Box className={styles.photoBanner} textAlign="center">
+              <Box>
+                <Typography
+                  variant="h4"
+                  style={{ fontFamily: "Fraunces", fontWeight: "900" }}
+                  gutterBottom
+                >
+                  Graphic Design
+                </Typography>
+                <Typography style={{ fontFamily: "Barlow" }} gutterBottom>
+                  Great design makes you memorable. We deliver artwork that
+                  underscores your brand message and captures potential clients’
+                  attention.
+                </Typography>
+                <Link>Learn more</Link>
+              </Box>
+            </Box>
+          </Grid>
+          <Grid className={styles.orangeBackground} item md={6} xs={12}>
+            <Box className={styles.photoBanner} textAlign="center">
+              <Typography
+                variant="h4"
+                style={{ fontFamily: "Fraunces", fontWeight: "900" }}
+                gutterBottom
+              >
+                Graphic Design
+              </Typography>
+              <Typography style={{ fontFamily: "Barlow" }} gutterBottom>
+                Great design makes you memorable. We deliver artwork that
+                underscores your brand message and captures potential clients’
+                attention.
+              </Typography>
+              <Link>Learn more</Link>
+            </Box>
+          </Grid>
+        </Grid>
+      </Box> */}
+
+      {/* fifth div  */}
+
+      <Box  textAlign="center">
+        <Box style={{margin:"200px 0 0 0"}}>
+            <Typography variant="h4">Client TestimonialsClient Testimonials</Typography>
+        </Box>
+      </Box>
+
     </Box>
   );
 };
